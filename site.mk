b3ba06363e1660d2c7ba74f1b35d0b5eba025ecf
vincent-ff-du <36100246+vincent-ff-du@users.noreply.github.com>
2018-05-02 08:02:01 +0200

    FF Niersufer (Sub MG) specific Gluon configuration
    FF Niersufer (Sub MG) specific Gluon configuration. 
    LEDE 2017.1.5 Beta rev.0.9.3

--- site.mk
+++ site.mk
@@ -1,36 +1,85 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-14 \
-        gluon-alfred \
-        gluon-announced \
-        gluon-autoupdater \
-        gluon-setup-mode \
-        gluon-config-mode-core \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-hostname \
-        gluon-config-mode-mesh-vpn \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-contact-info \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-mesh-vpn-fastd \
-        gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-        gluon-luci-wifi-config \
-        gluon-next-node \
-        gluon-mesh-vpn-fastd \
-        gluon-radvd \
-        gluon-status-page \
-        iwinfo \
-        iptables \
-        haveged
+	gluon-mesh-batman-adv-14 \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autorestart \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-source-filter \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-wifi-config \
+	gluon-web-private-wifi \
+	gluon-web-network \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
+	gluon-radvd \
+	gluon-setup-mode \
+	ffac-status-page \
+	gluon-ssid-changer \
+	iwinfo \
+	haveged
 
-GLUON_LANGS = en de
+USB_BASIC := \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid
 
-DEFAULT_GLUON_RELEASE := 0.7.4
+USB_NIC := \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-dm9601-ether
 
-# Allow overriding the release number from the command line
+USB_WIFI := \
+	kmod-rtl8192cu
+
+ifeq ($(GLUON_TARGET),x86-generic)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		kmod-usb-ohci-pci \
+		$(USB_NIC)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		kmod-igb #APU2
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
+
+DEFAULT_GLUON_RELEASE := 0.9.03
+
+
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
+GLUON_BRANCH ?= beta
+
+GLUON_LANGS ?= en de
+
+GLUON_ATH10K_MESH ?= ibss
+
+GLUON_REGION := eu
